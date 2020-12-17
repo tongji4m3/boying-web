8d5cd2387109c80bed704c01e81762f3425b9113
sjspingfandeshijie <1171011192@qq.com>
2020-12-17 16:54:57 +0800

    搜索页面，电脑马上没电了！！！

---
+++ src/views/Test.vue
@@ -0,0 +1,35 @@
+
+<template>
+    <el-radio-group v-model="radio" size="small" @change="changeTheme" fill="#66b1ff">
+        <el-radio-button :label="1">主题一</el-radio-button>
+        <el-radio-button :label="2">主题二</el-radio-button>
+        <el-radio-button :label="3">主题三</el-radio-button>
+    </el-radio-group>
+</template>
+<script>
+export default {
+    props: {},
+    data() {
+        return {
+
+            //主题切换
+            radio: 1,
+
+        };
+    },
+    computed: {},
+    created() {
+    },
+    mounted() {
+    },
+    watch: {},
+    methods: {
+        //切换主题
+        changeTheme(val) {
+            console.log(val)
+        },
+    },
+    destroyed() {
+    }
+};
+</script>

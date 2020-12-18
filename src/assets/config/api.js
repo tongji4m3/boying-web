252816ce0cc0d3c376cc7508171f2820b0bce952
sjspingfandeshijie <1171011192@qq.com>
2020-12-18 22:11:10 +0800

    api更新

--- src/assets/config/api.js
+++ src/assets/config/api.js
@@ -5,21 +5,23 @@ const APIRootUrl = 'http://47.103.203.188:8000/';
 
 module.exports = {
 
-    SearchShowUrl: APIRootUrl + 'show/search',
+    // 登录注册
     LoginUrl: APIRootUrl + 'user/usernameLogin',
     LoginTel1Url:APIRootUrl + 'user/telephoneLogin',
     LoginTel2Url:APIRootUrl + 'user/authCodeLogin',
     getAuthCodeUrl: APIRootUrl + 'user/getAuthCode',
     registerUrl: APIRootUrl + 'user/register',
-    getUserUrl: APIRootUrl + 'user/info',
 
+    // 分类相关
     getCategoryListUrl: APIRootUrl + 'category/categoryList',
     getCategoryMapUrl: APIRootUrl + 'category/categoryMap',
     getCategoryNameUrl: APIRootUrl + 'category/categoryName',
     searchUrl: APIRootUrl + 'show/search',
+    SearchShowUrl: APIRootUrl + 'show/search',
 
     // 个人信息
     getUserInfoUrl: APIRootUrl + 'user/info',
+    getUserUrl: APIRootUrl + 'user/info',
     updateUserInfoUrl: APIRootUrl + 'user/update',
     // 收货地址
     getAddressListUrl: APIRootUrl + 'address/list',

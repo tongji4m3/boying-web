2425efa1cfd52c72751a58e394628c197336dbf8
sjspingfandeshijie <1171011192@qq.com>
2020-12-19 12:31:41 +0800

    更新删除查看添加收货地址

--- src/views/SelfInformation.vue
+++ src/views/SelfInformation.vue
@@ -50,6 +50,7 @@
         <el-card>
             <br>
             默认收获地址：
+            <el-button type="primary" @click="showAddAddress()">添加收货地址</el-button>
             <el-table :data="defaultAddressList" style="width: 100%">
                 <el-table-column prop="province" label="省份"></el-table-column>
                 <el-table-column prop="city" label="城市"></el-table-column>
@@ -75,13 +76,82 @@
                 <el-table-column prop="details" label="详情"></el-table-column>
                 <el-table-column label="操作" width="400px">
                     <template slot-scope="scope">
-                        <el-button type="primary" @click="">查看详情</el-button>
-                        <el-button type="danger" @click="">删除</el-button>
+                        <el-button type="primary" @click="showAddress(scope.row.addressId)">查看详情</el-button>
+                        <el-button type="danger" @click="deleteAddress(scope.row.addressId)">删除</el-button>
                         <el-button type="info" @click="">编辑</el-button>
-                        <el-button type="success" @click="">设为默认</el-button>
+                        <el-button type="success" @click="setDefaultAddress(scope.row.addressId)">设为默认</el-button>
                     </template>
                 </el-table-column>
             </el-table>
+            <!--        添加活动对话框-->
+            <el-dialog title="添加收货地址" :visible.sync="addDialogVisible"
+                       width="630px" top="60px" center>
+                <!--            内容主体区域 放置一个表单-->
+                <!--绑定到addForm中，绑定验证规则对象addFormRules 表单校验项的引用为addFormRef-->
+                <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px">
+                    <!-- prop属性指定验证规则-->
+                    <el-form-item label="收货人:" prop="receiver">
+                        <!--v-model双向绑定-->
+                        <el-input style="width: 82%;" v-model="addForm.receiver"></el-input>
+                    </el-form-item>
+                    <el-form-item label="联系方式:" prop="phone">
+                        <el-input style="width: 82%;" v-model="addForm.phone"></el-input>
+                    </el-form-item>
+                    <el-form-item label="省:" prop="province">
+                        <el-input style="width: 82%;" v-model="addForm.province"></el-input>
+                    </el-form-item>
+                    <el-form-item label="市:" prop="city">
+                        <el-input style="width: 82%;" v-model="addForm.city"></el-input>
+                    </el-form-item>
+                    <el-form-item label="区:" prop="region">
+                        <el-input style="width: 82%;" v-model="addForm.region"></el-input>
+                    </el-form-item>
+                    <el-form-item label="街道:" prop="street">
+                        <el-input style="width: 82%;" v-model="addForm.street"></el-input>
+                    </el-form-item>
+                    <el-form-item label="详情:" prop="details">
+                        <el-input style="width: 82%;" type="textarea"
+                                  :autosize="{ minRows: 3, maxRows: 4}" v-model="addForm.details"></el-input>
+                    </el-form-item>
+                </el-form>
+                <!--            底部区域-->
+                <span slot="footer" class="dialog-footer">
+                    <el-button style="margin-right:20px" @click="cancelAdd()">取 消</el-button>
+                    <el-button style="margin-left:20px" type="primary" @click="addAddress()">确 定</el-button>
+                </span>
+            </el-dialog>
+            <!--        展示活动对话框-->
+            <el-dialog title="收货地址详情" :visible.sync="showDialogVisible" width="630px" top="60px" center>
+                <!--            内容主体区域 放置一个表单-->
+                <el-form :model="showForm" ref="addFormRef" label-width="100px">
+                    <!-- prop属性指定验证规则-->
+                    <el-form-item label="收货人:" prop="receiver">
+                        <!--v-model双向绑定-->
+                        <el-input style="width: 82%;" v-model="showForm.receiver" readonly="true"></el-input>
+                    </el-form-item>
+                    <el-form-item label="联系方式:" prop="phone">
+                        <el-input style="width: 82%;" v-model="showForm.phone" readonly="true"></el-input>
+                    </el-form-item>
+                    <el-form-item label="省:" prop="province">
+                        <el-input style="width: 82%;" v-model="showForm.province" readonly="true"></el-input>
+                    </el-form-item>
+                    <el-form-item label="市:" prop="city">
+                        <el-input style="width: 82%;" v-model="showForm.city" readonly="true"></el-input>
+                    </el-form-item>
+                    <el-form-item label="区:" prop="region">
+                        <el-input style="width: 82%;" v-model="showForm.region" readonly="true"></el-input>
+                    </el-form-item>
+                    <el-form-item label="街道:" prop="street">
+                        <el-input style="width: 82%;" v-model="showForm.street" readonly="true"></el-input>
+                    </el-form-item>
+                    <el-form-item label="详情:" prop="details">
+                        <el-input style="width: 82%;" type="textarea"
+                                  :autosize="{ minRows: 3, maxRows: 4}" v-model="showForm.details" readonly="true"></el-input>
+                    </el-form-item>
+                </el-form>
+            </el-dialog>
+
+
             <el-divider></el-divider>
             <!--            分页区域-->
             <el-pagination
@@ -93,6 +163,7 @@
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="totalCount">
             </el-pagination>
+
         </el-card>
     </div>
 </template>
@@ -122,6 +193,31 @@ export default {
 
             addressList: [],
             defaultAddressList: [],
+
+            addDialogVisible: false,
+            editDialogVisible: false,
+            showDialogVisible: false,
+
+            addForm: {
+                receiver: '',
+                phone: '',
+                province: '',
+                city: '',
+                region: '',
+                street: '',
+                details: '',
+            },
+            addFormRules: {},
+
+            showForm: {
+                receiver: '',
+                phone: '',
+                province: '',
+                city: '',
+                region: '',
+                street: '',
+                details: '',
+            },
         };
     },
     created() {
@@ -184,7 +280,7 @@ export default {
                     pageNum: this.pageNumber,
                     pageSize: this.pageSize,
                 });
-            console.log(result.data.data);
+            // console.log(result.data.data);
             if(result.data.code===200){
                 this.addressList=result.data.data.list;
             }
@@ -195,12 +291,8 @@ export default {
         },
         // 获取默认收货地址
         async getDefaultAddressList(){
-            var result = await this.$http.post(this.$api.getDefaultAddressUrl,
-                {
-                    pageNum: this.pageNumber,
-                    pageSize: this.pageSize,
-                });
-            console.log(result.data.data);
+            var result = await this.$http.post(this.$api.getDefaultAddressUrl);
+            console.log(result.data);
             if(result.data.code===200){
                 this.defaultAddressList=result.data.data.list;
             }
@@ -222,6 +314,70 @@ export default {
             this.pageNumber = newPage;
             // console.log("pageNumber:"+this.pageNumber);
             await this.getAddressList();
+        },
+        async updateAddress(id)
+        {
+            var result = await this.$http.post(this.$api.updateAddressUrl,id);
+        },
+        async deleteAddress(id)
+        {
+            // console.log(id);
+            await this.$http.post(this.$api.deleteAddressUrl + "/" + id);
+            this.getAddressList();
+            this.getDefaultAddressList();
+        },
+        async showAddAddress(){
+            this.addDialogVisible=true;
+        },
+        async addAddress(){
+            await this.$http.post(this.$api.addAddressUrl,{
+                receiver: this.addForm.receiver,
+                phone: this.addForm.phone,
+                province: this.addForm.province,
+                city: this.addForm.city,
+                region: this.addForm.region,
+                street: this.addForm.street,
+                details: this.addForm.details,
+            });
+            this.getAddressList();
+            this.addDialogVisible=false;
+
+            this.addForm.receiver='';
+            this.addForm.phone='';
+            this.addForm.province='';
+            this.addForm.city='';
+            this.addForm.region='';
+            this.addForm.street='';
+            this.addForm.details='';
+
+            // console.log(this.addForm);
+            this.$message.info("添加收货地点成功!");
+        },
+        async cancelAdd(){
+            this.addDialogVisible=false;
+
+            this.addForm.receiver='';
+            this.addForm.phone='';
+            this.addForm.province='';
+            this.addForm.city='';
+            this.addForm.region='';
+            this.addForm.street='';
+            this.addForm.details='';
+
+            // console.log(this.addForm);
+            this.$message.info("取消添加收货地点!");
+        },
+        async setDefaultAddress(id){
+            await this.$http.post(this.$api.setDefaultAddressUrl + "/" + id);
+            this.getAddressList();
+            this.getDefaultAddressList();
+        },
+
+        async showAddress(id){
+            let result = await this.$http.post(this.$api.getAddressUrl + "/" + id);
+            this.showForm=result.data.data;
+            // console.log(this.showForm);
+            this.showDialogVisible=true;
         },
     },
 

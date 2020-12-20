e9d4a23215d7ab517873644d64da134b202bfcf9
fh <1341710779@qq.com>
2020-12-20 23:19:17 +0800

    点击对应的演出目录跳转去对应的搜索界面完成，只剩下单界面了！

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,33 +36,56 @@
           </el-dropdown>
         </div>
       </el-header>
-      <el-main style="padding:0">
+      <el-main style="padding: 0">
         <!--            走马灯-->
         <el-carousel indicator-position="outside" height="400px">
           <el-carousel-item>
-              <img src="https://i.loli.net/2020/12/20/jI9Pqw5HUVEbSsr.jpg" height="400" width="1500" alt=""/>
+            <img
+              src="https://i.loli.net/2020/12/20/jI9Pqw5HUVEbSsr.jpg"
+              height="400"
+              width="1500"
+              alt=""
+            />
           </el-carousel-item>
-            <el-carousel-item>
-                <img src="https://i.loli.net/2020/12/20/phOzri862tuXZ9V.jpg" height="400" width="1500"/>
-            </el-carousel-item>
-            <el-carousel-item>
-                <img src="https://i.loli.net/2020/12/20/PgD9QxLN2JtnuXS.jpg" height="400" width="1500"/>
-            </el-carousel-item>
-            <el-carousel-item>
-                <img src="https://i.loli.net/2020/12/20/7AxHqzCvWItlSRj.jpg" height="400" width="1500"/>
-            </el-carousel-item>
+          <el-carousel-item>
+            <img
+              src="https://i.loli.net/2020/12/20/phOzri862tuXZ9V.jpg"
+              height="400"
+              width="1500"
+            />
+          </el-carousel-item>
+          <el-carousel-item>
+            <img
+              src="https://i.loli.net/2020/12/20/PgD9QxLN2JtnuXS.jpg"
+              height="400"
+              width="1500"
+            />
+          </el-carousel-item>
+          <el-carousel-item>
+            <img
+              src="https://i.loli.net/2020/12/20/7AxHqzCvWItlSRj.jpg"
+              height="400"
+              width="1500"
+            />
+          </el-carousel-item>
         </el-carousel>
         <!--            种类分类-->
         <el-card shadow="never">
           <el-row>
-            <el-col :span="3" v-for="category in categoryList">
+            <el-col
+              :span="3"
+              v-for="category in categoryList"
+              @click.native="search(category.categoryId)"
+            >
               <!--                    <el-card class="myCard" :body-style="{ padding: '20px'}" shadow="hover">-->
               <!--                        {{category}}-->
               <!--                    </el-card>-->
               <div style="text-align: center">
-                <i class="el-icon-share"></i>
+                <div class="categoryCol">
+                  <i class="el-icon-share"></i>
+                </div>
                 <br />
-                {{ category.name }}
+                <div class="categoryCol">{{ category.name }}</div>
               </div>
             </el-col>
           </el-row>
@@ -112,120 +135,124 @@
 <script>
 import axios from "axios";
 export default {
-    name: "Home",
-    data() {
-        return {
-            loading:true,
-            categoryCommand: 0,
-            categoryList: [],
-            showList: [],
-            translateList: [],
-            // translateList: ["演唱会","话剧歌剧","体育","展览休闲","音乐会","曲苑杂坛","舞蹈芭蕾","二次元"],
+  name: "Home",
+  data() {
+    return {
+      loading: true,
+      categoryCommand: 0,
+      categoryList: [],
+      showList: [],
+      translateList: [],
+      // translateList: ["演唱会","话剧歌剧","体育","展览休闲","音乐会","曲苑杂坛","舞蹈芭蕾","二次元"],
 
-            currentUser: {
-                userId: "",
-                icon: "",
-            },
-            categoryForm: {
-                categoryId: "",
-                parentId: "",
-                name: "",
-                weight: "",
-                description: "",
-            },
+      currentUser: {
+        userId: "",
+        icon: "",
+      },
+      categoryForm: {
+        categoryId: "",
+        parentId: "",
+        name: "",
+        weight: "",
+        description: "",
+      },
 
-            searchForm: {
-                categoryId: "",
-                pageNum: 1,
-                pageSize: 6,
-            },
-        };
-    },
-    created() {
-        this.getCategoryList();
-        this.getUser();
-        setTimeout(() => {
+      searchForm: {
+        categoryId: "",
+        pageNum: 1,
+        pageSize: 6,
+      },
+    };
+  },
+  created() {
+    this.getCategoryList();
+    this.getUser();
+    setTimeout(() => {
       this.loading = false;
     }, 500);
+  },
+  methods: {
+    search(id) {
+      this.$router.push({
+        path: "/search",
+        query: { categoryId: id },
+      });
     },
-    methods: {
-        toOrder() {
-            this.$router.push("/order");
-        },
-        logout(){
-            this.$router.push("/login");
-            this.$message.success("退出成功")
-        },
-        toSelfInformation(){
-            this.$router.push("/selfInformation");
-        },
-        async getCategoryName(id) {
-            let result = await this.$http.post(
-                this.$api.getCategoryNameUrl,
-                JSON.stringify(id+1)
-            );
-            // console.log(result.data.data);
-            return result.data.data;
-        },
-        async getCategoryList() {
-            let result = await this.$http.post(
-                this.$api.getCategoryListUrl,
-                JSON.stringify(0)
-            );
-            // console.log(result)
-            this.categoryList = result.data.data;
-            // console.log(this.categoryList)
-            this.showList.length=this.categoryList.length;
-            this.translateList.length=this.categoryList.length;
-            for (var i = 0; i < this.categoryList.length; i++) {
-                this.showList[i] = await this.getShow(this.categoryList[i].categoryId);
-                this.translateList[i] = await this.getCategoryName(this.categoryList[i].categoryId-1);
-                // this.showList[i].name = this.categoryList[i].name;
-            }
-            console.log(this.showList);
-        },
-        async getShow(categoryId) {
-            let result = await this.$http.post(
-                this.$api.searchUrl,
-                JSON.stringify({
-                    categoryId: categoryId,
-                    pageNum: 1,
-                    pageSize: 6,
-                }));
-
-            // console.log(result);
-            this.$forceUpdate();
-            if(result.data.code===200)
-            {
-                // for (let i = 0; i < result.data.data.list.length; i++)
-                // {
-                //     result.data.data.list.length[i].dayEnd=result.data.data.list.length[i].dayEnd.substring(0,10);
-                //     result.data.data.list.length[i].dayStart=result.data.data.list.length[i].dayStart.substring(0,10);
-                // }
-                console.log(result);
-                return result.data.data.list;
-            }
-
-            else
-                return [];
-        },
-
-        async getUser() {
-            try {
-                const res = await axios.post(this.$api.getUserUrl);
-                console.log(res);
-                if (res.data.code === 200) {
-                    this.currentUser = res.data.data;
-                }
-                else{
-                    this.currentUser.userId='';
-                }
-            } catch (err) {
-                console.log(err);
-                this.$message.error("获取用户信息失败");
-            }
-        },
+    toOrder() {
+      this.$router.push("/order");
     },
+    logout() {
+      this.$router.push("/login");
+      this.$message.success("退出成功");
+    },
+    toSelfInformation() {
+      this.$router.push("/selfInformation");
+    },
+    async getCategoryName(id) {
+      let result = await this.$http.post(
+        this.$api.getCategoryNameUrl,
+        JSON.stringify(id + 1)
+      );
+      // console.log(result.data.data);
+      return result.data.data;
+    },
+    async getCategoryList() {
+      let result = await this.$http.post(
+        this.$api.getCategoryListUrl,
+        JSON.stringify(0)
+      );
+      // console.log(result)
+      this.categoryList = result.data.data;
+      // console.log(this.categoryList)
+      this.showList.length = this.categoryList.length;
+      this.translateList.length = this.categoryList.length;
+      for (var i = 0; i < this.categoryList.length; i++) {
+        this.showList[i] = await this.getShow(this.categoryList[i].categoryId);
+        this.translateList[i] = await this.getCategoryName(
+          this.categoryList[i].categoryId - 1
+        );
+        // this.showList[i].name = this.categoryList[i].name;
+      }
+      console.log(this.showList);
+    },
+    async getShow(categoryId) {
+      let result = await this.$http.post(
+        this.$api.searchUrl,
+        JSON.stringify({
+          categoryId: categoryId,
+          pageNum: 1,
+          pageSize: 6,
+        })
+      );
+
+      // console.log(result);
+      this.$forceUpdate();
+      if (result.data.code === 200) {
+        // for (let i = 0; i < result.data.data.list.length; i++)
+        // {
+        //     result.data.data.list.length[i].dayEnd=result.data.data.list.length[i].dayEnd.substring(0,10);
+        //     result.data.data.list.length[i].dayStart=result.data.data.list.length[i].dayStart.substring(0,10);
+        // }
+        console.log(result);
+        return result.data.data.list;
+      } else return [];
+    },
+
+    async getUser() {
+      try {
+        const res = await axios.post(this.$api.getUserUrl);
+        console.log(res);
+        if (res.data.code === 200) {
+          this.currentUser = res.data.data;
+        } else {
+          this.currentUser.userId = "";
+        }
+      } catch (err) {
+        console.log(err);
+        this.$message.error("获取用户信息失败");
+      }
+    },
+  },
 };
 </script>
 
@@ -276,5 +303,10 @@ export default {
 
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
+}
+
+.categoryCol {
+  cursor: pointer; /*鼠标悬停变小手*/
+  background-clip: padding-box;
 }
 </style>
